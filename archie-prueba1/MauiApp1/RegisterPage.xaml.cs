e7e07eb181079e36f0d8907ff3670ded02707823
Angel Poveda <angeldanicp@gmail.com>
2026-09-15 11:29:17 -0500

    Fix missing RegisterPage.xaml.cs from coworker commit

---
+++ archie-prueba1/MauiApp1/RegisterPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using MauiApp1.Services;
+using MauiApp1.Models;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnBackClicked(object? sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        private void OnToggleRegPasswordVisibility(object? sender, EventArgs e)
+        {
+            RegPasswordEntry.IsPassword = !RegPasswordEntry.IsPassword;
+            RegPasswordToggleIcon.Source = RegPasswordEntry.IsPassword ? "ic_eye_off.svg" : "ic_eye_on.svg";
+        }
+
+        private async void OnSubmitRegisterClicked(object? sender, EventArgs e)
+        {
+            // Lógica pendiente de mover (Actualmente funciona en el bottom sheet de LoginPage)
+            await DisplayAlert("Aviso", "El registro ha sido migrado al panel principal por ahora.", "OK");
+            await Navigation.PopModalAsync();
+        }
+    }
+}
