50c7429c11e316d2189e0ed3f1c366f797fddcc0
Angel Poveda <angeldanicp@gmail.com>
2026-09-18 10:29:58 -0500

    feat: custom floating pill tab bar matching design

---
+++ archie-prueba1/MauiApp1/Controls/FloatingTabBar.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace MauiApp1.Controls;
+
+public partial class FloatingTabBar : ContentView
+{
+    public static readonly BindableProperty SelectedIndexProperty =
+        BindableProperty.Create(nameof(SelectedIndex), typeof(int), typeof(FloatingTabBar), 0, propertyChanged: OnSelectedIndexChanged);
+
+    public int SelectedIndex
+    {
+        get => (int)GetValue(SelectedIndexProperty);
+        set => SetValue(SelectedIndexProperty, value);
+    }
+
+    public FloatingTabBar()
+    {
+        InitializeComponent();
+        UpdateVisualStates(SelectedIndex);
+    }
+
+    private static void OnSelectedIndexChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is FloatingTabBar control)
+        {
+            control.UpdateVisualStates((int)newValue);
+        }
+    }
+
+    private async void OnTabTapped(object sender, TappedEventArgs e)
+    {
+        if (e.Parameter is string param && int.TryParse(param, out int index))
+        {
+            if (SelectedIndex == index) return;
+            
+            UpdateVisualStates(index);
+            
+            string route = index switch
+            {
+                0 => "//DashboardPage",
+                1 => "//DesignPage",
+                2 => "//MainPage",
+                3 => "//ProfilePage",
+                _ => "//DashboardPage"
+            };
+            
+            await Shell.Current.GoToAsync(route, false);
+        }
+    }
+
+    private void UpdateVisualStates(int index)
+    {
+        var activeBg = Color.FromArgb("#334155");
+        var inactiveBg = Colors.Transparent;
+
+        Bg0.BackgroundColor = inactiveBg; Label0.IsVisible = false; Icon0.Opacity = 0.5;
+        Bg1.BackgroundColor = inactiveBg; Label1.IsVisible = false; Icon1.Opacity = 0.5;
+        Bg2.BackgroundColor = inactiveBg; Label2.IsVisible = false; Icon2.Opacity = 0.5;
+        Bg3.BackgroundColor = inactiveBg; Label3.IsVisible = false; Icon3.Opacity = 0.5;
+
+        switch (index)
+        {
+            case 0:
+                Bg0.BackgroundColor = activeBg; Label0.IsVisible = true; Icon0.Opacity = 1.0;
+                break;
+            case 1:
+                Bg1.BackgroundColor = activeBg; Label1.IsVisible = true; Icon1.Opacity = 1.0;
+                break;
+            case 2:
+                Bg2.BackgroundColor = activeBg; Label2.IsVisible = true; Icon2.Opacity = 1.0;
+                break;
+            case 3:
+                Bg3.BackgroundColor = activeBg; Label3.IsVisible = true; Icon3.Opacity = 1.0;
+                break;
+        }
+    }
+}
